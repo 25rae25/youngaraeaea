aec0523f78c34ec0ef2097c6c9dffe3319a21922
booldook <booldook@gmail.com>
2020-06-08 17:49:54 +0900

    booldook

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
-$(".header .bars").click(onBarClick)
+$(".header .bars").click(onBarClick);
 
 function onBarClick() {
-	if($(this).hasClass("default")) {
-		$(this).removeClass("default").addClass("active");
-		fixShow(true);
-	}
-	else if($(this).hasClass("active")) {
-		$(this).removeClass("active").addClass("default");
-		fixShow(false);
+	if($(this).hasClass("active")) {
+		$(this).removeClass("active");
+		$(this).find(".bar1").css("animation-name", "bar1-rev");
+		$(this).find(".bar2").css("animation-name", "bar2-rev");
+		$(this).find(".bar3").css("animation-name", "bar3-rev");
 	}
 	else {
 		$(this).addClass("active");
-		fixShow(true);
+		$(this).find(".bar1").css("animation-name", "bar1");
+		$(this).find(".bar2").css("animation-name", "bar2");
+		$(this).find(".bar3").css("animation-name", "bar3");
 	}
 }
